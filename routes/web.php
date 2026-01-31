d255d687743dcc68674f4ad885176acda4889f0a
fallndoumbe <ndoumbefall163@gmail.com>
2026-01-31 13:00:43 +0000

    personnalisation des vues d'authentification

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,8 +18,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-Route::middleware(['auth'])->group(function () {
-    Route::resource('tasks', TaskController::class);
-    Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggleComplete'])
-         ->name('tasks.toggle');
-});
